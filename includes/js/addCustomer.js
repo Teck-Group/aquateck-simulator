d8b0e3257a1eba260416236226df4d42d17bc918
Teck-belgium <skyteck.belgium@gmail.com>
2026-02-23 18:34:10 +0100

    Update addCustomer.js

--- includes/js/addCustomer.js
+++ includes/js/addCustomer.js
@@ -2,60 +2,40 @@ console.log("addCustomer.js geladen");
 
 import { auth, db } from "/aquateck-simulator/includes/firebase/config.js";
 import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { createUserWithEmailAndPassword, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {sendSignInLinkToEmail } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
   const form = document.getElementById("customerForm");
   const melding = document.getElementById("melding");
 
-  if (!form) {
-    console.error("Form not found");
-    return;
-  }
+  const actionCodeSettings = {
+    url: "https://teck-bel.github.io/aquateck-simulator/finishSignUp.html",
+    handleCodeInApp: true
+  };
 
   form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
-    const nameInput = document.getElementById("name").value;
-    const emailInput = document.getElementById("email").value;
-    const companyInput = document.getElementById("company").value;
-    const simulatorInput = document.getElementById("simulator").value;
-
-    if (!nameInput || !emailInput || !companyInput || !simulatorInput) {
-      console.error("One of the fields doesn't exists");
-      return;
-    }
-
-    
-    const name =nameInput.value;
-    const email = emailInput.value;
-    const company = companyInput.value;
-    const simulator = simulatorInput.value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const company = document.getElementById("company").value;
+    const simulator = document.getElementById("simulator").value;
 
     try {
-      // temporary random password generate
-      const randomPassword = Math.random().toString(36).slice(-10);
+      //send login link
+      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
 
-      // Create user in firebase auth
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        randompassword
-        );
+      localStorage.setItem("emailForSignIn", email);
 
-      const uid = userCredential.user.uid;
-
-      // send reset password email so user can choose his own password
-      await senPasswaordResetEmail(auth, email);
-
-      //Save client in firestore database
+      //Create a firestrore document
       await addDoc(collection(db, "customers"), {
-        uid,
         name,
         email,
         company,
         simulator,
+        role: "customer",
+        licenseActive: true,
         aangemaaktOp: serverTimestamp()
       });
 
